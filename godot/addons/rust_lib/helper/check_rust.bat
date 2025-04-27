e2589fd79cc2d7a0558999baae170ce3efff995b
robotnik-dev <robotnik.games@gmail.com>
2025-04-27 18:19:36 +0200

    fix: update scripts and assets for Rust integration
    - Set CHANGES_DETECTED variable correctly in cargo_diff.bat
    - Improve error messages in check_rust.bat and install_rust.bat
    - Refactor RustLibManager to use new process handling
    - Update scene files and add reload icon asset

--- godot/addons/rust_lib/helper/check_rust.bat
+++ godot/addons/rust_lib/helper/check_rust.bat
@@ -4,12 +4,11 @@ setlocal
 :: Check if rustc (Rust compiler) is available
 where rustc >nul 2>nul
 if %ERRORLEVEL% neq 0 (
-    echo Rust is NOT installed. 1>&2
+    echo Rust is not installed.
     exit /b 1
 ) else (
     :: Display installed Rust version
     echo Rust is installed.
-    echo Current rustc version
     rustc --version
     exit /b 0
 )
--- godot/addons/rust_lib/helper/install_rust.bat
+++ godot/addons/rust_lib/helper/install_rust.bat
@@ -4,14 +4,14 @@ setlocal
 :: Download and install Rustup (which installs Rust)
 curl --silent --show-error --location --output rustup-init.exe https://win.rustup.rs
 if %ERRORLEVEL% neq 0 (
-    echo Failed to download rustup installer. 1>&2
+    echo Failed to download rustup installer.
     exit /b 1
 )
 
 :: Run the Rustup installer
 rustup-init.exe -y
 if %ERRORLEVEL% neq 0 (
-    echo Rust installation failed. 1>&2
+    echo Rust installation failed.
     exit /b 1
 )
 
